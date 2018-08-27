35bdd954d18b92db3218dc713db1ccc0e243f408
subayadhav <suba10499@gmail.com>
2018-08-27 13:50:23 +0530

    Create sumk.java

---
+++ sumk.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class sumk
+{
+  public static void main(String args[])
+{
+Scanner in=new Scanner(System.in);
+int N=in.nextInt();
+int K=in.nextInt();
+sum=0;
+int a[]=new int[N];
+for(i=0;i<a.length;i++)
+{
+  a[i]=nextInt();
+}
+for(i=0;i<k;i++)
+{
+  sum=sum+a[i];
+}
+System.out.println("sum");
+}
+}
