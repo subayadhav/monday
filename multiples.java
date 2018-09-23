c8791c1ea8a241d8148c3e58b0486e7abacc70d8
subayadhav <suba10499@gmail.com>
2018-09-23 13:37:16 +0530

    Create multiples.java

---
+++ multiples.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class Multiples
+{
+public static void main(String args[])
+{
+System.out.println("enter a number");
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+for(int i=1;i<=5;i++)
+{
+System.out.println(n*i);
+}
+}
+}
